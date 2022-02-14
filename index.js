6ae545a9272d5f28b2eaafc62c51a28409b35859
Jacob Hooser <jacob.hooser@yahoo.com>
2022-02-14 14:31:18 -0600

    Completed assignment

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    string = string.toUpperCase();
+    console.log(string);
+    //return string;
+}
+function logWhisper(string) {
+    string = string.toLowerCase();
+    console.log(string);
+    //return string;
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (string == string.toLowerCase()) {
+        return "I can't hear you!";
+    }
+    else if (string == string.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    else if (string == "Let's have dinner together!"){
+        return "I would love to!";
+    }
+}
